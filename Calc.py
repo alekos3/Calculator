f1c70ded6b0c7c5c2758ca2395ba1efefc19ff46
Alexios Nersessian <79263622+alekos3@users.noreply.github.com>
2023-06-26 13:15:40 -0400

    Create Calc.py
    Initial Commit.

---
+++ Calc.py
@@ -0,0 +1,19 @@
+# Calculator
+
+
+def addition(x, y):
+    result = x + y
+    return result
+
+
+def subtraction(x, y):
+    result = x - y
+    return result
+
+
+def division(x, y):
+    pass
+
+
+def multiplication(x, y):
+    pass
